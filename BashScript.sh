cdb8335dfdd7a4f982d1cc63bb38f637ae72c6f0
mariatafoya7 <49257711+mariatafoya7@users.noreply.github.com>
2019-06-04 19:55:11 -0700

    Update BashScript.sh

--- BashScript.sh
+++ BashScript.sh
@@ -2,5 +2,6 @@
 
 perl -pe 's/(.*)\;\n/\1Nothing\n/g' $1 | grep ';' | grep -v 'Nothing' | perl -pe 's/;;/;Nothing;/g' | perl -pe 's/\;/\t/g' | awk '{ print $10, $11 }' > thing.txt
 
+Rscript script.R
 
 #end
