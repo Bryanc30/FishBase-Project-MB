39deee35a53198926d1e72fa3cef2d095195b26f
mariatafoya7 <49257711+mariatafoya7@users.noreply.github.com>
2019-06-06 14:23:52 -0700

    Update BashScript.sh

--- BashScript.sh
+++ BashScript.sh
@@ -1,5 +1,8 @@
 #/!/bin/bash
 
+. /u/local/Modules/default/init/modules.sh
+module load R
+
 echo "beginning the process"
 
 perl -pe 's/(.*)\;\n/\1Nothing\n/g' $1 | grep ';' | grep -v 'Nothing' | perl -pe 's/;;/;Nothing;/g' | perl -pe 's/\;/\t/g' | awk '{ print $10, $11 }' > thing.txt
